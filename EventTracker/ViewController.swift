53f51d24b295cddcc97dbe0c0a74bf34c6c5659d
Jack Bransfield <jjbii@builtlight.org>
2021-04-13 12:04:35 -0500

    Initial Commit

---
+++ EventTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventTracker
+//
+//  Created by Jack Bransfield on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
